b2e56ae3570da1079eae039cae39ff86c136212e
Brian-Junior <xr100bj@gmail.com>
2023-06-16 18:39:08 -0400

    first commit

---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package projects.exception;
+
+/**
+ * @author xr100
+ *
+ */
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+	
+	
+
+	/**
+	 * @param message
+	 */
+	public DbException(String message) {
+		super(message);
+
+
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DbException(Throwable cause) {
+		super(cause);
+
+
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+
+
+	}
+}
